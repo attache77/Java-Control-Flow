90789cc5b72c1f18cc086021e11eb520c7ff5dbe
Attache Hardy <attachehardy@gmail.com>
2020-12-01 20:38:50 -0500

    This is my java control commit

---
+++ Java Control Flow/src/javacontrolflowhomework.java
@@ -0,0 +1,28 @@
+
+public class javacontrolflowhomework {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("Do you have a red car?");
+		System.out.println("What is the name of your favorite pet?");
+		System.out.println("What is the age of your pet?");
+		System.out.println("What is your lucky number?");
+		System.out.println("Do you have a favorite quarterback?  If so what is their jersey number");
+		System.out.println("What is the 2 digit model number of your car?");
+		System.out.println("What is the first name of your favorite actor or actress?");
+		System.out.println("enter a random number betweein 1 and 50?");
+		
+		int x = 50;
+		int y = 20;
+		if (x == y) {
+			System.out.println("Magic Ball");
+		} else if (x > y) {
+			System.out.println("Lottery Winner");
+		} else {
+			System.out.println("Lottery Loser");
+		}
+	}
+}
+	
+
+
---
+++ Java Control Flow/src/javacontrolflowtutorial.java
@@ -0,0 +1,36 @@
+
+public class javacontrolflowtutorial {
+
+	public static void main(String[] args) {
+		int x = 50;
+		int y = 20;
+		if (x == y) {
+			System.out.println("1");
+		} else if (x > y) {
+			System.out.println("2");
+		} else {
+			System.out.println("3");
+		
+		//int x = 50;
+		//int y = 20;
+		//if (x == y) {
+			//System.out.println("Hello World!");
+		
+		//int x = 50;
+		//int y = 10;
+		//if (x > y) {
+			//System.out.println("Hello World!");
+		}
+		//int time = 20;
+		//String result; 
+		//result = (time < 18) ? "Good day." : "Good Evening.";
+		//System.out.println(result);
+	}
+}
+		
+		
+		
+		
+		
+	
+
